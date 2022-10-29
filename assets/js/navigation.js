45da5fb95332cd99365efed44fcdbf9a1a171339
ndaboom <ndabosam084@gmail.com>
2022-10-29 17:37:38 +0200

    Navigation added

---
+++ assets/js/navigation.js
@@ -0,0 +1,34 @@
+const optionsMenu = document.querySelectorAll('.menu-option');
+const aboutView = document.querySelector('.about-view');
+const listView = document.querySelector('.list-view');
+const formView = document.querySelector('.books-crud');
+const homeTitle = document.querySelector('.home-title');
+const screensArr = [listView, formView, aboutView];
+
+function removeActive() {
+  optionsMenu.forEach((option) => {
+    option.classList.remove('blue');
+  });
+}
+
+function manageScreens(i) {
+  console.log("Current element index", i);
+  screensArr.forEach((option) => {
+    option.classList.add('hidden');
+  });
+  if(i === 0){
+    homeTitle.classList.remove('hidden');
+  } else {
+    homeTitle.classList.add('hidden');
+  }
+  screensArr[i].classList.remove('hidden');   
+}
+
+// Add click listener to menu options
+optionsMenu.forEach((option, index) => {
+  option.addEventListener('click', (e) => {
+    removeActive();
+    manageScreens(index);
+    option.classList.add('blue');
+  });
+});
